5e8197fbbe70c5ed35752851bffcd3d039aa5a95
48amubake <48amubake@cr.k12.ia.us>
2016-11-30 08:58:03 -0600

    importanat files

---
+++ Assets/script/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Controller2D))]
+public class Player : MonoBehaviour {
+
+
+	Controller2D controller;
+
+	void Start() {
+		controller = GetComponent<Controller2D> ();
+	}
+
+	void update () {
+	
+		Vector2 input = new Vector2(Input.GetAxisRaw (''horizontal''
+		Velocity.y += gravity * Time.deltaTime;
+	controller.Move(velocity* Time.deltaTime);
+	...
+}
